0d343fdb43f65cd6713fdbda7ff8d9a913826f65
Vasutevar <tevarvasu@gmail.com>
2023-05-17 11:31:36 -0400

    1st commit

---
+++ Week2Demo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author HP15-EK0025TX
+ */
+public class Week2Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
